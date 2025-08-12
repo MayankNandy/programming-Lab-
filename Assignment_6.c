d2f29af3c39f2703ce46bdba0e7e7b69944d4cf9
MayankNandy <mayanknandy06@gmail.com>
2025-08-12 21:35:38 +0530

    Add files via upload

---
+++ Assignment_6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter a character to get it's ASCII Code ");
+    scanf("%c",&ch); 
+    int a= ch;
+    printf("The ASCII code of the character is:%d",a);
+    return 0;
+}
